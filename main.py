45405a5d8eb29432f42178a19be666f458481b04
Piyush dixit <70336252+Er-luffy-D@users.noreply.github.com>
2024-07-12 12:58:47 +0000

    added ping command and error msg.

--- main.py
+++ main.py
@@ -1,2 +1,28 @@
-import discord
+from logging import disable
 import os
+import discord
+from discord.ext import commands
+from discord.flags import Intents
+# Set up the bot
+with open('token.txt', "r") as f:
+  Token = f.readline()
+client = commands.Bot(command_prefix=".", intents=discord.Intents.all())
+
+
+# Define the command to send a message
+@client.command()
+async def ping(ctx):
+  await ctx.send(f'yay! {round(client.latency*1000)}ms')
+
+
+client.remove_command("help")
+# Embeded help command 
+
+
+
+#If there is an error, it will answer with an error
+@client.event
+async def on_command_error(ctx, error):
+    await ctx.send(f'Error. Try .help ({error})')
+
+client.run(Token)
