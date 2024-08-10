f3e2f7511af220d9e14ccf23855634afb5a81e18
Piyush dixit <70336252+Er-luffy-D@users.noreply.github.com>
2024-08-10 10:52:08 +0000

    working on anime commands

--- commands/anime.py
+++ commands/anime.py
@@ -1,1 +1,17 @@
-# List of Animes
+from AnilistPython import Anilist
+import requests
+
+
+def ongoing():
+  response = requests.get("https://myanimelist.net/anime/season")
+  # response = response.json()
+  return response.content
+
+
+def search(anime_name):
+  anime = Anilist()
+  return anime.get_anime(anime_name)
+
+
+if __name__ == "__main__":
+  print(ongoing())
